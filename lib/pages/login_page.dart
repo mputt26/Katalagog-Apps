ee428e32ff52ef903b321ddb78c2789e04db6890
Putri Diana Hafsyawati <101302312+mputt26@users.noreply.github.com>
2023-11-01 00:08:41 +0700

    create ne ui

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:menu_resto/data/api_service_user.dart';
 import 'package:menu_resto/pages/home_page.dart';
+import 'package:menu_resto/pages/register_page.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -13,6 +14,8 @@ class _LoginPageState extends State<LoginPage> {
   final ApiUser apiUser = ApiUser();
   TextEditingController usernameController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  String? _errorText;
 
   void _login() async {
     String username = usernameController.text;
@@ -25,108 +28,171 @@ class _LoginPageState extends State<LoginPage> {
         MaterialPageRoute(builder: (context) => HomePage(name: username)),
       );
     } else {
-      _showErrorDialog();
+      setState(() {
+        _errorText = 'Invalid username or password. Please try again.';
+      });
     }
-  }
-
-  void _showErrorDialog() {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('Invalid Credentials'),
-          content: Text('Please enter valid username and password.'),
-          actions: <Widget>[
-            TextButton(
-              child: Text('OK'),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-          ],
-        );
-      },
-    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       resizeToAvoidBottomInset: false,
-      backgroundColor: Colors.black,
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           SingleChildScrollView(
             child: Container(
-              padding: EdgeInsets.only(left: 50, right: 50, top: 200),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('LOGIN',
-                      style: TextStyle(
-                          fontSize: 40,
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold)),
-                  SizedBox(height: 20),
-                  usernameForm(),
-                  SizedBox(height: 20),
-                  passwordForm(),
-                  SizedBox(height: 20),
-                  submitLogin(),
-                ],
+              padding: EdgeInsets.only(left: 50, right: 50),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Log In',
+                        style: TextStyle(
+                            fontSize: 35,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold)),
+                    SizedBox(height: 40),
+                    usernameForm(),
+                    SizedBox(height: 20),
+                    passwordForm(),
+                    SizedBox(height: 20),
+                    if (_errorText != null)
+                      Padding(
+                        padding: EdgeInsets.only(bottom: 10),
+                        child: Text(
+                          _errorText!,
+                          style: TextStyle(color: Colors.red),
+                        ),
+                      ),
+                    submitLogin(),
+                    SizedBox(height: 20),
+                    registerAccount()
+                  ],
+                ),
               ),
             ),
           ),
-          Spacer(),
-          Container(
-              width: double.infinity,
-              height: 300,
-              child: Image.asset(
-                'assets/images/design2.png',
-                fit: BoxFit.fill,
-              ))
         ],
       ),
     );
   }
 
   Widget usernameForm() {
-    return Form(
+    return Container(
+      decoration: BoxDecoration(
+        color: const Color.fromARGB(255, 232, 232, 232),
+        borderRadius: BorderRadius.circular(25),
+      ),
       child: TextFormField(
         controller: usernameController,
-        style: TextStyle(color: Colors.white),
+        style: TextStyle(color: const Color.fromARGB(255, 74, 74, 74)),
         keyboardType: TextInputType.name,
         decoration: InputDecoration(
           labelText: 'Username',
-          labelStyle: TextStyle(color: Colors.white),
-          prefixIcon: Icon(Icons.account_circle, color: Colors.white),
+          labelStyle: TextStyle(color: Colors.grey.shade500),
+          prefixIcon:
+              Icon(Icons.account_circle_outlined, color: Colors.grey.shade500),
+          border: InputBorder.none,
         ),
+        validator: (value) {
+          if (value == null || value.isEmpty) {
+            return 'Please enter your username';
+          }
+          return null;
+        },
       ),
     );
   }
 
   Widget passwordForm() {
-    return TextFormField(
-      controller: passwordController,
-      obscureText: true,
-      style: TextStyle(color: Colors.white),
-      keyboardType: TextInputType.name,
-      decoration: InputDecoration(
+    return Container(
+      decoration: BoxDecoration(
+        color: const Color.fromARGB(255, 232, 232, 232),
+        borderRadius: BorderRadius.circular(25),
+      ),
+      child: TextFormField(
+        controller: passwordController,
+        obscureText: true,
+        style: TextStyle(color: const Color.fromARGB(255, 74, 74, 74)),
+        keyboardType: TextInputType.name,
+        decoration: InputDecoration(
           labelText: 'Password',
-          labelStyle: TextStyle(color: Colors.white),
-          prefixIcon: Icon(Icons.lock, color: Colors.white)),
+          labelStyle: TextStyle(color: Colors.grey.shade500),
+          prefixIcon: Icon(Icons.lock_outline, color: Colors.grey.shade500),
+          border: InputBorder.none,
+        ),
+        validator: (value) {
+          if (value == null || value.isEmpty) {
+            return 'Please enter your password';
+          }
+          return null;
+        },
+      ),
     );
   }
 
   Widget submitLogin() {
-    return ElevatedButton(
-      onPressed: _login,
-      style: ElevatedButton.styleFrom(
-        textStyle: TextStyle(fontSize: 20),
+    return SizedBox(
+      width: double.infinity,
+      child: ElevatedButton(
+        onPressed: () {
+          if (_formKey.currentState?.validate() ?? false) {
+            _login();
+          }
+        },
+        style: ElevatedButton.styleFrom(
+          primary: Colors.orange,
+          textStyle: TextStyle(
+              fontSize: 20, color: Colors.white, fontWeight: FontWeight.w600),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(40),
+          ),
+          padding: EdgeInsets.symmetric(vertical: 15),
+        ),
+        child: Text('Log In'),
       ),
-      child: Text('Submit'),
+    );
+  }
+
+  Widget registerAccount() {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Register()),
+              );
+            },
+            child: Text(
+              "Do you haven't account? ",
+              style: TextStyle(color: Colors.black, fontSize: 14),
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Register()),
+              );
+            },
+            child: Text(
+              "Register Now!",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 14),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }

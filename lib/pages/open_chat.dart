6e61006282c6bd12c00c7051de3f6c92bca48c9e
Putri Diana Hafsyawati <101302312+mputt26@users.noreply.github.com>
2023-11-01 00:11:36 +0700

    chatbot test and fixed

--- lib/pages/open_chat.dart
+++ lib/pages/open_chat.dart
@@ -1,106 +1,139 @@
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
 
-class OpenChat extends StatefulWidget {
-  const OpenChat({Key? key}) : super(key: key);
+class Message {
+  final String sender;
+  final String text;
 
-  @override
-  _OpenChatState createState() => _OpenChatState();
+  Message({required this.sender, required this.text});
 }
 
-class _OpenChatState extends State<OpenChat> {
-  List<ChatMessage> messages = [];
+void main() => runApp(MaterialApp(home: ChatBot()));
 
-  void _handleSubmitted(String text) {
+class ChatBot extends StatefulWidget {
+  @override
+  State<ChatBot> createState() => _ChatBotState();
+}
+
+class _ChatBotState extends State<ChatBot> {
+  final List<Message> _messages = [];
+  TextEditingController _messageController = TextEditingController();
+
+  void _sendMessage(String text) {
     setState(() {
-      messages.add(ChatMessage(
-        text: text,
-        isUserMessage: true,
-      ));
-      // Di sini Anda dapat menambahkan logika untuk mengirim pesan ke chatbot AI
-      // Dan mendapatkan balasan dari chatbot AI
-      // Misalnya: messages.add(ChatMessage(text: 'Balasan dari chatbot', isUserMessage: false));
+      _messages.add(Message(sender: 'User', text: text));
     });
+    _askGPT(text);
+  }
+
+  void _askGPT(String question) async {
+    // Ganti dengan kunci API OpenAI yang valid
+    String apiKey = '';
+    Dio dio = Dio(BaseOptions(
+      baseUrl: 'https://api.openai.com/v1',
+      headers: {
+        'Authorization': 'Bearer $apiKey',
+        'Content-Type': 'application/json',
+      },
+    ));
+
+    Map<String, dynamic> data = {
+      "model": "gpt-3.5-turbo",
+      "messages": [
+        {
+          "role": "system",
+          "content":
+              "Kamu adalah Sava, seorang Chef handal yang akan memberikan rekomendasi makanan dan minuman yang ada di Restaurant Savoria, dibawah ini adalah beberapa makanan yang ada di Savoria. Dragonfire Grilled Chicken : Dragonfire Grilled Chicken adalah ayam panggang dengan saus pedas eksotis yang terbuat dari campuran cabai, rempah-rempah, dan madu. Disajikan dengan nasi goreng bawang putih dan sayuran rebus. Midnight Truffle Pasta : Midnight Truffle Pasta adalah pasta hitam yang diolah dengan saus krim truffle lembut, jamur porcini, dan parmesan. Zen Garden Sushi Bowl : Zen Garden Sushi Bowl adalah campuran sushi rice, potongan ikan salmon segar, alpukat, mentimun, dan tobiko. Diberi sentuhan saus ponzu yang menyegarkan dan taburan biji wijen. Vampire Slayer Burger : Vampire Slayer Burger adalah burger daging sapi panggang dengan saus bawang putih dan keju Swiss meleleh. Disajikan dengan kentang goreng truffle. Phoenix Rising Spicy Ramen : Phoenix Rising Spicy Ramen adalah mie ramen pedas dengan kuah kaldu tulang sapi yang kaya rasa, potongan daging sapi empuk, jamur shiitake, dan telur rebus. Disajikan dengan irisan daun bawang dan nori. Golden Tempura Udon : Golden Tempura Udon adalah mie udon hangat dengan kuah dashi, disertai dengan udang tempura garing, lobak, wortel, dan daun bawang"
+        },
+        {
+          "role": "user",
+          "content": question,
+        }
+      ]
+    };
+
+    try {
+      Response response = await dio.post(
+          'https://api.openai.com/v1/chat/completions',
+          data: data,
+          options: Options(contentType: Headers.jsonContentType));
+      setState(() {
+        _messages.add(Message(
+            sender: 'ChatBot',
+            text: response.data['choices'][0]['message']['content']));
+      });
+      print('Response: ${response.data.toString()}');
+    } on DioError catch (e) {
+      print(e.message);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Savoria Bot'),
-        backgroundColor: Color.fromARGB(255, 66, 58, 50),
-        toolbarHeight: 65,
+        title: Text('ChatBot'),
       ),
       body: Column(
         children: <Widget>[
-          Flexible(
+          Expanded(
             child: ListView.builder(
-              padding: EdgeInsets.all(8.0),
-              reverse: true,
-              itemCount: messages.length,
-              itemBuilder: (_, int index) => messages[index],
+              itemCount: _messages.length,
+              itemBuilder: (context, index) {
+                var message = _messages[index];
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Align(
+                    alignment: message.sender == 'User'
+                        ? Alignment.topRight
+                        : Alignment.topLeft,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: message.sender == 'User'
+                            ? Colors.blue
+                            : Colors.green,
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      padding: EdgeInsets.all(10.0),
+                      child: Text(
+                        message.text,
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                );
+              },
             ),
           ),
-          _buildTextComposer(),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildTextComposer() {
-    return Container(
-      margin: EdgeInsets.all(8.0),
-      child: Row(
-        children: <Widget>[
-          Flexible(
-            child: Padding(
-              padding: const EdgeInsets.only(right: 8.0),
-              child: Container(
-                padding: EdgeInsets.all(12.0),
-                decoration: BoxDecoration(
-                  color: Colors.grey[300],
-                  borderRadius: BorderRadius.circular(30.0),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: TextField(
+                    controller: _messageController,
+                    onSubmitted: (text) {
+                      _sendMessage(text);
+                      _messageController.clear();
+                    },
+                    decoration: InputDecoration(
+                      hintText: 'Type a message...',
+                      contentPadding: EdgeInsets.symmetric(
+                          vertical: 10.0, horizontal: 20.0),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                      ),
+                    ),
+                  ),
                 ),
-                child: TextField(
-                  onSubmitted: _handleSubmitted,
-                  decoration:
-                      InputDecoration.collapsed(hintText: 'Send a message'),
+                IconButton(
+                  icon: Icon(Icons.send),
+                  onPressed: () {
+                    _sendMessage(_messageController.text);
+                    _messageController.clear();
+                  },
                 ),
-              ),
-            ),
-          ),
-          IconButton(
-            icon: Icon(Icons.send),
-            onPressed: () => _handleSubmitted('User message'),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class ChatMessage extends StatelessWidget {
-  final String text;
-  final bool isUserMessage;
-
-  ChatMessage({required this.text, required this.isUserMessage});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.symmetric(vertical: 10.0),
-      child: Row(
-        mainAxisAlignment:
-            isUserMessage ? MainAxisAlignment.end : MainAxisAlignment.start,
-        children: <Widget>[
-          Container(
-            decoration: BoxDecoration(
-              color: isUserMessage ? Colors.blue : Colors.grey,
-              borderRadius: BorderRadius.circular(8.0),
-            ),
-            padding: EdgeInsets.all(10.0),
-            child: Text(
-              text,
-              style: TextStyle(color: Colors.white),
+              ],
             ),
           ),
         ],

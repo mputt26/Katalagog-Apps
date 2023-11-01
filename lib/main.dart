270ba06207c6115613355afea10913837a0524c2
Putri Diana Hafsyawati <101302312+mputt26@users.noreply.github.com>
2023-11-01 12:52:09 +0700

    add provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:menu_resto/pages/opening_page.dart';
+import 'package:menu_resto/viewModel/register.dart';
+import 'package:menu_resto/data/api_service_regis.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        Provider<MockApi>(create: (_) => MockApi()),
+        ChangeNotifierProvider<RegisterViewModel>(
+          create: (context) =>
+              RegisterViewModel(Provider.of<MockApi>(context, listen: false)),
+        ),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  
-  // This widget is the root of your application.
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
